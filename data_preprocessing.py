fc03b11679b97eb198d0d19f867252f841f95fc5
Arnon Puitrakul <peter_arnon1@icloud.com>
2018-05-22 00:40:25 +0700

    Add Imputation functions

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import Imputer
 import pandas as pd
 
 def read_data (path) :
@@ -5,15 +6,8 @@ def read_data (path) :
 
 def prepare_multiple_line_data (dataset) :
     dataset = impute_icu_dischage_status(dataset)
-    dataset = impute_case_status_in_multiple_line(dataset)
+    dataset = impute_ventilator_day_in_multiple_line (dataset)
     dataset = normalise_last_data_case_status_in_multiple_line(dataset)
-    return dataset
-
-def impute_case_status_in_multiple_line (dataset) :
-    # Impute all record which record_day < num_of_record_day to be 0 (Stay in ICU)
-    for index,row in dataset.iterrows() :
-        if row.record_day < row.num_of_record_day :
-            row.case_status = 0
     return dataset
 
 def normalise_last_data_case_status_in_multiple_line (dataset) :
@@ -31,5 +25,18 @@ def impute_icu_dischage_status (dataset) :
     dataset.icu_discharge_status = dataset.icu_discharge_status.replace(2,1)
     return dataset
 
+def impute_ventilator_day_in_multiple_line (dataset) :
+    for index,row in dataset.iterrows() :
+        if row.total_ventilator_day > record_day :
+            row.total_ventilator_day = record_day
+    return dataset
+
 def impute_boolean (dataset) :
-    return dataset.replace({'No':False, 'Yes':True})
+    return dataset.replace({'No':False, 'No(Aki)':False, 'Yes':True, 'na':None, 'NA':None,'n':None})
+
+def drop_columns (dataset, columns_to_be_dropped=[]) :
+    # Drop all specific columns
+    return dataset.drop(columns=columns_to_be_dropped)
+
+def impute_data (dataset) :
+    return Imputer().fit_transform(dataset)
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import data_preprocessing
+import model_trainer
 
 def main () :
     multiple_line_dataset = data_preprocessing.read_data('./Data/multiple-line-record.csv')
+    multiple_line_dataset = data_preprocessing.impute_boolean(multiple_line_dataset)
     multiple_line_dataset = data_preprocessing.prepare_multiple_line_data(multiple_line_dataset)
+    multiple_line_dataset = data_preprocessing.drop_columns(multiple_line_dataset, ['birth_date', 'date_of_admission', 'date_of_hospital_discharge', 'date_of_icu_admission', 'date_of_icu_discharge', 'date_of_true_baseline_serum_cr', 'death_date', 'first_available_cr_date', 'move_out_time', 'stop_rrt_date', 'time_of_icu_admission'])
+    multiple_line_dataset = multiple_line_dataset.fillna(method='backfill')
 
+    print (multiple_line_dataset.isnull().sum())
+    # Prepare Feature Space and Label
+    multiple_line_features = data_preprocessing.impute_data(multiple_line_dataset.drop(columns=['case_status', 'available_baseline', 'diagnosis', 'type_of_vascular_access', 'internal_jugular_lr', 'dialyzer', 'complication_ihd', 'type_of_vascular_access_sled', 'dialyzer_sled', 'internal_jugular_lr_crrt', 'type_solution', 'site', 'indication', 'vasopressor_type', 'site_of_vascular_access', 'site_of_vascular_access_sled', 'patient_number', 'patient_order', 'hospital_discharge_status', 'icu_discharge_status']))
+    multiple_line_label = multiple_line_dataset.case_status
+
+    # multiple_line_dataset.to_csv('./Data/multiple-line-record-transform.csv', sep=",", na_rep='', index=False)
+
+    model = model_trainer.build_gb_model(multiple_line_features, multiple_line_label)
 if __name__ == "__main__" :
     main()
+
+
+# gender, ht, dm, arterial_ph, chronic_health_points, rs, renal, aki_max_staging_from, cr_diagnosis_from_day, cause_of_aki, reimbursement, baseline_serum_creainine_by_mdrd, age, record_day, creatinine, urine_output_cal
